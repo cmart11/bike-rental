fa94f0427436d64708f36f1ea9c9532f10e87038
Cristian Martinez <2cristianmartinez@gmail.com>
2019-09-13 17:14:39 -0400

    working express server

--- server/index.js
+++ server/index.js
@@ -12,14 +12,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // logging middleware
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     // res.sendFile(path.join(__dirname, '..', 'views'));
-    res.render('index.ejs', { bikes: data.products });
+    try {
+        res.render('index.ejs', { bikes: data.products });
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.get('/:bikeId', (req, res, next) => {
+    try {
+        const bikeId = +req.params.bikeId;
+        const bike = data.products.filter(el => el.id === bikeId)[0];
+        console.log('bike', bike);
+        res.render('index.ejs', { bicycle: bike });
+    } catch (error) {
+        next(error);
+    }
 });
 
 // error handling endware
